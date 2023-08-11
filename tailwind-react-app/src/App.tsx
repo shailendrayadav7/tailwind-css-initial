16b2326bebd7c98722b50a2b8b2af67561840e8a
shailendrayadav7 <sailendrayadav80@gmail.com>
2023-08-11 15:36:49 -0500

    UI Changes

--- tailwind-react-app/src/App.tsx
+++ tailwind-react-app/src/App.tsx
@@ -7,27 +7,41 @@ const App: React.FC = () => {
   const [submitDisabled, setSubmitDisabled] = useState(false);
 
   const handleFormSubmit = (data: any) => {
-    console.log(data); 
-    setSubmitDisabled(true); 
+    console.log(data);
+    setSubmitDisabled(true);
   };
 
   return (
     <div className="p-8">
-      <Button type="primary" onClick={() => alert('Primary button clicked')}>
-        Primary Button
-      </Button>
-      <Button type="secondary" onClick={() => alert('Secondary button clicked')}>
-        Secondary Button
-      </Button>
-      <Button type="outlined" onClick={() => alert('Outlined button clicked')}>
-        Outlined Button
-      </Button>
+      <h1 className="text-2xl font-semibold mb-4">Animal List</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <Button type="primary" onClick={() => alert('Primary button clicked')}>
+          Domestic Animal
+        </Button>
+        <Button type="secondary" onClick={() => alert('Secondary button clicked')}>
+          Wild Animal
+        </Button>
+        <Button type="outlined" onClick={() => alert('Outlined button clicked')}>
+          Tertiary Animal
+        </Button>
+      </div>
 
-      <Card>
-        <h2 className="text-lg font-semibold">Tailwind CSS</h2>
-        <p>Setting up Tailwind CSS in a Vite project. </p>
-      </Card>
+      <h1 className="text-2xl font-semibold mb-4">Domestic Animal</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <Card>
+          <h2 className="text-lg font-semibold">Dog</h2>
+          <img src="src\assets\images\dog.jpg" alt="Card Image" className="w-full h-auto mb-4" />
+          <p>This is Yeezy.</p>
+        </Card>
 
+        <Card>
+          <h2 className="text-lg font-semibold">Cat</h2>
+          <img src="src\assets\images\cat.jpg" alt="Card Image" className="w-full h-auto mb-4" />
+          <p>This is Lucy.</p>
+        </Card>
+      </div>
+
+      <h1 className="text-2xl font-semibold mb-4">User Form</h1>
       <Form onSubmit={handleFormSubmit} submitDisabled={submitDisabled} />
     </div>
   );
